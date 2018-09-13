dc4518d814b35e2fd2ea1130a315202a52462b29
Michael Hucka <mhucka@caltech.edu>
2018-09-13 10:06:33 -0700

    Add some common methods

--- handprint/htr/base.py
+++ handprint/htr/base.py
@@ -3,4 +3,11 @@ htr/base.py: base class definition for HTR systems.
 '''
 
 class HTR(object):
-    pass
+    def __init__(self):
+        pass
+
+    def init_credentials(self):
+        pass
+
+    def text_from(self, path):
+        pass

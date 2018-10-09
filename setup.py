1ae2ab55eef6ad6f0cb3677f4cfc9248e992de54
Michael Hucka <mhucka@caltech.edu>
2018-10-09 09:53:08 -0700

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    setup.py
+# @brief   Handprint setup file
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/handprint
+# =============================================================================
+
+import os
+from   os import path
+from   setuptools import setup
+import sys
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as f:
+    reqs = f.read().rstrip().splitlines()
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+# The following reads the variables without doing an "import handprint",
+# because the latter will cause the python execution environment to fail if
+# any dependencies are not already installed -- negating most of the reason
+# we're using setup() in the first place.  This code avoids eval, for security.
+
+version = {}
+with open(path.join(here, 'handprint/__version__.py')) as f:
+    text = f.read().rstrip().splitlines()
+    vars = [line for line in text if line.startswith('__') and '=' in line]
+    for v in vars:
+        setting = v.split('=')
+        version[setting[0].strip()] = setting[1].strip().replace("'", '')
+
+# Finally, define our namesake.
+
+setup(
+    name             = version['__title__'].lower(),
+    description      = version['__description__'],
+    long_description = readme,
+    version          = version['__version__'],
+    url              = version['__url__'],
+    author           = version['__author__'],
+    author_email     = version['__email__'],
+    license          = version['__license__'],
+    keywords         = "archives OCR HTR library",
+    packages         = ['handprint'],
+    scripts          = ['bin/handprint'],
+    install_requires = reqs,
+    platforms        = 'any',
+    python_requires  = '>=3',
+)

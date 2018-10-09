187c666ee8ce2a66f20e7422fb164cba127bac4d
Michael Hucka <mhucka@caltech.edu>
2018-10-08 17:10:45 -0700

    Add Handprint-specific exceptions

---
+++ handprint/exceptions.py
@@ -0,0 +1,27 @@
+'''
+exceptions.py: exceptions defined by Handprint
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+class UserCancelled(Exception):
+    '''The user elected to cancel/quit the program.'''
+    pass
+
+class ServiceFailure(Exception):
+    '''Unrecoverable problem involving network services.'''
+    pass
+
+class InternalError(Exception):
+    '''Unrecoverable problem involving Handprint itself.'''
+    pass

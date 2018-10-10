81c086273ef33c5dd118c0e2791384ba21a9f656
Michael Hucka <mhucka@caltech.edu>
2018-10-10 12:04:05 -0700

    Catch exceptions when talking to Google

--- handprint/htr/google.py
+++ handprint/htr/google.py
@@ -5,13 +5,19 @@ google.py: interface to Google HTR network service
 import io
 import os
 from os import path
+import google
 from google.cloud import vision_v1p3beta1 as vision
+from google.api_core.exceptions import PermissionDenied
 
 import handprint
 from handprint.credentials.google_auth import GoogleCredentials
+from handprint.messages import msg
+from handprint.exceptions import ServiceFailure
 from handprint.debug import log
 
+
 from .base import HTR
+
 
 
 # Main class.
@@ -45,6 +51,9 @@ class GoogleHTR(HTR):
                                                       image_context = context)
             if __debug__: log('Received result.')
             return response.full_text_annotation.text
+        except google.api_core.exceptions.PermissionDenied as err:
+            text = 'Authentication failure for Google service -- {}'.format(err)
+            raise ServiceFailure(text)
         except Exception as err:
             text = 'Error: failed to convert "{}": {}'.format(path, err)
             return text

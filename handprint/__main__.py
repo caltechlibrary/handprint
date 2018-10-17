d9b552afe7351391c94b2745c97b4375ec2eec9c
Michael Hucka <mhucka@caltech.edu>
2018-10-16 17:30:52 -0700

    Huge refactoring + allow images located at URLs

--- handprint/__main__.py
+++ handprint/__main__.py
@@ -25,176 +25,294 @@ from   os import path
 import plac
 import requests
 import sys
+from   sys import exit as exit
 try:
     from termcolor import colored
 except:
     pass
 import time
 import traceback
+from   urllib import request
+import wget
 
 import handprint
-from handprint.messages import msg, color
+from handprint.constants import ON_WINDOWS, IMAGE_FORMATS, KNOWN_METHODS
+from handprint.messages import msg, color, MessageHandlerCLI
 from handprint.network import network_available
 from handprint.files import files_in_directory, replace_extension, handprint_path
-from handprint.files import readable
+from handprint.files import readable, writable
 from handprint.htr import GoogleHTR
 from handprint.htr import MicrosoftHTR
 from handprint.exceptions import *
 from handprint.debug import set_debug, log
 
-
-# Global constants.
-# .............................................................................
-
-_IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
-
-_METHODS = {
-    'google': GoogleHTR,
-    'microsoft': MicrosoftHTR,
-}
-
-
 # Main program.
 # ......................................................................
 
 @plac.annotations(
-    credsdir = ('look for credentials files in directory "D"', 'option', 'c'),
-    list     = ('print list of known HTR methods',             'flag',   'l'),
-    method   = ('use HTR method "M" (default: "all")',         'option', 'm'),
-    quiet    = ('do not print messages while working',         'flag',   'q'),
-    no_color = ('do not color-code terminal output',           'flag',   'C'),
-    debug    = ('turn on debugging (console only)',            'flag',   'D'),
-    version  = ('print version info and exit',                 'flag',   'V'),
-    files    = 'directories and/or files to process',
+    creds_dir  = ('look for credentials files in directory "D"',  'option', 'c'),
+    from_file  = ('read file names or URLs from file "F"',        'option', 'f'),
+    list       = ('print list of known methods',                  'flag',   'l'),
+    method     = ('use method "M" (default: "all")',              'option', 'm'),
+    given_urls = ('assume have URLs, not files (default: files)', 'flag',   'u'),
+    output     = ('write output to directory "O"',                'option', 'o'),
+    quiet      = ('do not print messages while working',          'flag',   'q'),
+    no_color   = ('do not color-code terminal output',            'flag',   'C'),
+    debug      = ('turn on debugging (console only)',             'flag',   'D'),
+    version    = ('print version info and exit',                  'flag',   'V'),
+    images     = 'if given -u, URLs, else directories and/or files',
 )
 
-def main(credsdir = 'D', list = False, method = 'M', quiet = False,
-         no_color = False, debug = False, version = False, *files):
+def main(creds_dir = 'D', from_file = 'F', list = False, method = 'M',
+         output = 'O', given_urls = False, quiet = False, no_color = False,
+         debug = False, version = False, *images):
     '''Handprint (a loose acronym of "HANDwritten Page RecognitIoN Test") can
 run alternative optical character recognition (OCR) and handwritten text
-recognition (HTR) methods on documents.
+recognition (HTR) methods on images of document pages.
 
-When invoked, the command-line arguments should end with either one or more
-directory names or one or more image files.  Handprint will send each image
-file found to OCR/HTR services from Google, Microsoft and others, and write the
-text returned by them to text files in the same directories as the images.
-The text files will be placed in new directories named after the service being
-used; e.g., "microsoft" for the results of using Microsoft's Azure API.
+When invoked, the command-line arguments should contain one of the following:
+
+ a) one or more directory paths or one or more image file paths, which will
+    be interpreted as images (either individually or in directories) to be
+    processed;
+
+ b) if given the -u option (/u on Windows), one or more URLs, which will be
+    interpreted as network locations of image files to be processed;
+
+ c) if given the -f option (/f on Windows), a file containing either image
+    paths or (if combined with the -u option), image URLs
+
+Handprint will send each image file (or URL) to OCR/HTR services from Google,
+Microsoft and others.  It will write the results to new files placed either in
+the same directories as the original files or if given the -o option (/o on
+Windows), to the directory indicated by the argument to the -o option.  The
+results will be written in files named after the original files with the
+addition of a string that indicates the service used.  For example, a file
+named "someimage.jpg" will produce
+
+  someimage.jpg
+  someimage.google.txt
+  someimage.microsoft.txt
+  someimage.amazon.txt
+  ...
+
+and so on for each image and each service used.  Note that if -u (/u on
+Windows) is given, then an output directory must also be specified using the
+option -o (/o on Windows) because it is not possible to write the results in
+the network locations represented by the URLs.
 
 If given the command-line flag -l (or /l on Windows), Handprint will print a
 list of the known methods and then exit.  The option -m (/m on Windows) can
 be used to select a specific method.  (The default method is "google".)
 
+Note: the only image formats recognized are JPG, PNG, GIF, and BMP.
+
+Credentials for different services need to be provided to Handprint in the
+form of JSON files.  Each service needs a separate JSON file named after the
+service (e.g., "microsoft.json") and placed in a directory that Handprint
+searches.  By default, Handprint searches for the files in a subdirectory
+named "creds" where Handprint is installed, but an alternative directory can
+be indicated at run-time using the -c command-line option (/c on Windows).
+The specific format of each credentials file is different for each service;
+please consult the Handprint documentation for more details.
+
 If given the -V option (/V on Windows), this program will print version
 information and exit without doing anything else.
 '''
 
-    # Our defaults are to do things like color the output, which means the
-    # command line flags make more sense as negated values (e.g., "no-color").
-    # However, dealing with negated variables in our code is confusing, so:
-    use_color   = not no_color
+    # Prepare notification methods and hints.
+    notifier = MessageHandlerCLI(not no_color)
+    hint = '(Hint: use /h for help.)' if ON_WINDOWS else '(Hint: use -h for help.)'
 
     # Process arguments.
     if debug:
         set_debug(True)
     if version:
         print_version()
-        sys.exit()
+        exit()
     if list:
-        msg('Known methods (for use as values for option -m):', 'info', use_color)
-        for key in _METHODS.keys():
-            msg('   {}'.format(key), 'info', use_color)
-        sys.exit()
-    if not files:
-        raise SystemExit(color('No directories or files given', 'error', use_color))
+        notifier.info('Known methods:')
+        for key in KNOWN_METHODS.keys():
+            notifier.info('   {}'.format(key))
+        exit()
     if not network_available():
-        raise SystemExit(color('No network', 'error', use_color))
-    if credsdir == 'D':
-        credsdir = path.join(handprint_path(), 'creds')
-    if not readable(credsdir):
-        raise SystemExit(color('"{}" not readable'.format(credsdir), 'error', use_color))
+        exit(notifier.fatal_text('No network.'))
+
+    if from_file == 'F':
+        from_file = None
     else:
-        if __debug__: log('Assuming credentials found in {}', credsdir)
+        if not path.isabs(from_file):
+            from_file = path.realpath(path.join(os.getcwd(), from_file))
+        if not path.exists(from_file):
+            exit(notifier.error_text('File not found: {}'.format(from_file)))
+        if not readable(from_file):
+            exit(notifier.error_text('File not readable: {}'.format(from_file)))
+
+    if creds_dir == 'D':
+        creds_dir = path.join(handprint_path(), 'creds')
+    if not readable(creds_dir):
+        exit(notifier.error_text('Directory not readable: {}'.format(creds_dir)))
+    else:
+        if __debug__: log('Assuming credentials found in "{}".', creds_dir)
+
     if method == 'M':
         method = 'all'
-    if method != 'all' and method.lower() not in _METHODS:
-        msg('"{}" is not a known method'.format(method), 'error', use_color)
-        sys.exit()
-    if not quiet:
-        from halo import Halo
+    method = method.lower()
+    if method != 'all' and method not in KNOWN_METHODS:
+        exit(notifier.error_text('"{}" is not a known method. {}'.format(method, hint)))
+
+    if not images and not from_file:
+        exit(notifier.error_text('Need provide directories, files or URLs. {}'.format(hint)))
+    if any(item.startswith('-') for item in images):
+        exit(notifier.error_text('Unrecognized option in arguments. {}'.format(hint)))
+
+    if output == 'O':
+        output = None
+    else:
+        if not path.isabs(output):
+            output = path.realpath(path.join(os.getcwd(), output))
+        if path.isdir(output):
+            if not writable(output):
+                exit(notifier.error_text('Directory not writable: {}'.format(output)))
+        else:
+            os.mkdir(output)
+            if __debug__: log('Created output directory "{}"', output)
+    if given_urls and not output:
+        exit(notifier.error_text('Must provide an output directory if using URLs.'))
 
     # Create a list of files to be processed.
-    files_list = []
-    for item in files:
-        if path.isfile(item) and path.splitext(item)[1] in _IMAGE_EXTENSIONS:
-            files_list.append(item)
-        elif path.isdir(item):
-            files_list += files_in_directory(item, extensions = _IMAGE_EXTENSIONS)
-        else:
-            msg('"{}" not a file or directory'.format(item), 'warn', use_color)
+    targets = []
+    if from_file:
+        with open(from_file) as f:
+            targets = f.readlines()
+        targets = [line.rstrip('\n') for line in targets]
+        if __debug__: log('Read {} lines from "{}".', len(targets), from_file)
+        if not given_urls:
+            targets = filter_urls(targets, notifier)
+    elif given_urls:
+        # We assume that the arguments are URLs and take them as-is.
+        targets = images
+    else:
+        # We were given files and/or directories.  Look for image files.
+        for item in filter_urls(images, notifier):
+            if path.isfile(item) and path.splitext(item)[1] in IMAGE_FORMATS:
+                targets.append(item)
+            elif path.isdir(item):
+                targets += files_in_directory(item, extensions = IMAGE_FORMATS)
+            else:
+                notifier.warn('"{}" not a file or directory'.format(item))
+    if not targets:
+        exit(notifier.warn_text('No images to process; quitting.'))
 
     # Let's do this thing.
-    methods = _METHODS.values() if method == 'all' else [_METHODS[method.lower()]]
     if method == 'all':
-        msg('Applying all methods in succession.', 'info', use_color)
-    for method_object in methods:
-        if method == 'all':
-            msg('='*70, 'dark', use_color)
-        run(method_object, files_list, credsdir, use_color, quiet, debug)
-    if method == 'all':
-        msg('='*70, 'dark', use_color)
-    msg('Done.', 'info', use_color)
+        notifier.info('Applying all methods in succession.')
+        for m in KNOWN_METHODS.values():
+            notifier.msg('='*70, 'dark')
+            run(m, targets, given_urls, output, creds_dir, notifier, quiet, debug)
+        notifier.msg('='*70, 'dark')
+    else:
+        run(KNOWN_METHODS[method], targets, given_urls, output, creds_dir, notifier, quiet, debug)
+    notifier.info('Done.')
 
 
 # If this is windows, we want the command-line args to use slash intead
 # of hyphen.
 
-if sys.platform.startswith('win'):
+if ON_WINDOWS:
     main.prefix_chars = '/'
 
 
 # Helper functions.
 # ......................................................................
 
-def run(method_object, files_list, credsdir, use_color, quiet, debug):
+def run(method_class, targets, given_urls, output_dir, creds_dir,
+        notifier, quiet, debug):
     spinner = None
     try:
-        tool = method_object()
-        msg('Using method "{}".'.format(tool.name()), 'info', use_color)
-        tool.init_credentials(credsdir)
-        for file in files_list:
-            if use_color and not quiet:
-                spinner = Halo(spinner='bouncingBall', text = color(file, 'info'))
+        tool = method_class()
+        notifier.info('Using method "{}".'.format(tool.name()))
+        tool.init_credentials(creds_dir)
+        for index, item in enumerate(targets, 1):
+            if not given_urls and (item.startswith('http') or item.startswith('ftp')):
+                notifier.warn('Skipping URL "{}"'.format(item))
+                continue
+            if notifier.use_color() and not quiet:
+                spinner = Halo(spinner='bouncingBall', text = color(item, 'info'))
                 spinner.start()
-            full_path = path.realpath(path.join(os.getcwd(), file))
+            if given_urls:
+                # Make sure the URLs point to images.
+                response = request.urlopen(item)
+                if response.headers.get_content_maintype() != 'image':
+                    spinner.fail(notifier.error_text(
+                        'Did not find an image at "{}"'.format(item)))
+                    continue
+                format = response.headers.get_content_subtype()
+                if format not in IMAGE_FORMATS:
+                    spinner.fail(notifier.error_text(
+                        'Cannot use image format {} in "{}"'.format(format, item)))
+                    continue
+                # If we're given URLs, we have to invent file names to store
+                # the images and the OCR results.
+                base = 'url-{}'.format(index)
+                url_file = path.realpath(path.join(output_dir, base + '.url'))
+                if __debug__: log('Writing URL to {}', url_file)
+                with open(url_file, 'w') as f:
+                    f.write(url_file_content(item))
+                if __debug__: log('Starting wget on {}', item)
+                import pdb; pdb.set_trace()
+                downloaded = wget.download(item, bar = None, out = output_dir)
+                file = path.realpath(path.join(output_dir, base + '.' + format))
+                if __debug__: log('Renaming downloaded file to {}', file)
+                os.rename(downloaded, file)
+                full_path = file
+            else:
+                file = item
+                full_path = path.realpath(path.join(os.getcwd(), file))
             file_name = path.basename(full_path)
-            dest_dir = path.join(path.dirname(full_path), tool.name())
-            if not path.exists(dest_dir):
-                if __debug__: log('Creating {}', dest_dir)
-                os.makedirs(dest_dir)
-            dest_file = replace_extension(path.join(dest_dir, file_name), '.txt')
+            if output_dir:
+                dest_dir = output_dir
+            else:
+                dest_dir = path.dirname(full_path)
+                if not writable(dest_dir):
+                    notifier.fatal('Cannot write output in "{}".'.format(dest_dir))
+                    return
+            dest_file = replace_extension(path.join(dest_dir, file_name),
+                                          '.' + tool.name() + '.txt')
             save_output(tool.text_from(file), dest_file)
-            if use_color and not quiet:
+            if notifier.use_color() and not quiet:
                 short_path = path.relpath(dest_file, os.getcwd())
                 spinner.succeed(color('{} -> {}'.format(file, short_path), 'info'))
                 spinner.stop()
     except (KeyboardInterrupt, UserCancelled) as err:
         if spinner:
             spinner.stop()
-        msg('Quitting.', 'info', use_color)
-        sys.exit()
+        exit(notifier.info_text('Quitting.'))
     except ServiceFailure as err:
         if spinner:
-            spinner.fail(color('Stopping due to a problem', 'error', use_color))
-        msg(err, 'error', use_color)
-        sys.exit()
+            spinner.fail(notifier.error_text('Stopping due to a problem'))
+        exit(notifier.error_text(err))
     except Exception as err:
         if spinner:
-            spinner.fail(color('Stopping due to a problem', 'error', use_color))
+            spinner.fail(notifier.error_text('Stopping due to a problem'))
         if debug:
             import pdb; pdb.set_trace()
-        msg('{}\n{}'.format(str(err), traceback.format_exc()), 'error', use_color)
-        sys.exit()
+        exit(notifier.error_text('{}\n{}'.format(str(err), traceback.format_exc())))
+
+
+def filter_urls(item_list, notifier):
+    results = []
+    for item in item_list:
+        if item.startswith('http') or item.startswith('ftp'):
+            notifier.warn('Unexpected URL: "{}"'.format(item))
+            continue
+        else:
+            results.append(item)
+    return results
+
+
+def url_file_content(url):
+    return '[InternetShortcut]\nURL={}\n'.format(url)
 
 
 def print_version():

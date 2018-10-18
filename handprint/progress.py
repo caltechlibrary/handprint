0345697cd33838c87e3d526dca9972397ee02358
Michael Hucka <mhucka@caltech.edu>
2018-10-18 15:59:12 -0700

    Add wrapper around Spinner for indicating progress

---
+++ handprint/progress.py
@@ -0,0 +1,91 @@
+'''
+progress.py: show indication of progresss
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from   halo import Halo
+from   pubsub import pub
+import sys
+import time
+import wx
+import wx.lib.dialogs
+
+try:
+    from termcolor import colored
+    if sys.platform.startswith('win'):
+        import colorama
+        colorama.init()
+except:
+    pass
+
+import handprint
+from handprint.messages import color, msg
+from handprint.debug import log
+
+
+class ProgressIndicator():
+
+    def __init__(self, colorize, quiet):
+        self._colorize = colorize
+        self._quiet = quiet
+        self._current_message = ''
+
+
+    def start(self, message = None):
+        if self._quiet:
+            return
+        if message is None:
+            message = ''
+        if self._colorize:
+            text = color(message, 'info')
+            self._spinner = Halo(spinner='bouncingBall', text = text)
+            self._spinner.start()
+            self._current_message = message
+        else:
+            msg(color(message, 'info'))
+
+
+    def update(self, message = None):
+        if self._quiet:
+            return
+        if self._colorize:
+            self._spinner.succeed(color(self._current_message, 'info', self._colorize))
+            self._spinner.stop()
+            self.start(message)
+        else:
+            msg(color(message, 'info'))
+
+
+    def stop(self, message = None):
+        if self._quiet:
+            return
+        if self._colorize:
+            self._spinner.succeed(color(self._current_message, 'info', self._colorize))
+            self._spinner.stop()
+            self.start(message)
+            self._spinner.succeed(color(message, 'info', self._colorize))
+            self._spinner.stop()
+        else:
+            msg(color(message, 'info'))
+
+
+    def fail(self, message = None):
+        if self._colorize:
+            self._spinner.fail(color(self._current_message, 'error', self._colorize))
+            self._spinner.stop()
+            self.start(message)
+            self._spinner.fail(color(message, 'error', self._colorize))
+            self._spinner.stop()
+        else:
+            msg(color(message, 'error'))

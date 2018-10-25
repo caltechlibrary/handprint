1145e45d1ccd03567172e63f17dfa98e28cbfb0b
Michael Hucka <mhucka@caltech.edu>
2018-10-25 15:13:23 -0700

    Change text_from() -> document_text(), plus add all_results()

--- handprint/htr/base.py
+++ handprint/htr/base.py
@@ -6,11 +6,22 @@ class HTR(object):
     def __init__(self):
         pass
 
+
     def init_credentials(self):
+        '''Initializes the credentials to use for accessing this service.'''
         pass
 
-    def text_from(self, path):
-        pass
 
     def name(self):
+        '''Returns the canonical internal name for this service.'''
+        pass
+
+
+    def document_text(self, path):
+        '''Returns the pure text extracted from the image by this service.'''
+        pass
+
+
+    def all_results(self, path):
+        '''Returns all the results from the service as a Python dict.'''
         pass

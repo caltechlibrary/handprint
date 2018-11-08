eb65b4f70c020f7e2f1c46cf7686212e5564dd99
Michael Hucka <mhucka@caltech.edu>
2018-11-07 18:32:20 -0800

    Update info & add a screencast

--- handprint/__version__.py
+++ handprint/__version__.py
@@ -6,7 +6,7 @@
 # @website https://github.com/caltechlibrary/handprint
 # =============================================================================
 
-__version__     = '0.7.3'
+__version__     = '0.7.4'
 __title__       = 'handprint'
 __description__ = 'Handprint: HANDwritten Page RecognitIoN Test for Caltech Archives'
 __url__         = 'https://github.com/caltechlibrary/handprint'

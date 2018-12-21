3adeb86aeeac6de51749eb8843e63ca3812ecf81
Michael Hucka <mhucka@caltech.edu>
2018-12-21 13:41:49 -0800

    Version bump for next version

--- handprint/__version__.py
+++ handprint/__version__.py
@@ -6,7 +6,7 @@
 # @website https://github.com/caltechlibrary/handprint
 # =============================================================================
 
-__version__     = '0.8.2'
+__version__     = '0.8.3'
 __title__       = 'handprint'
 __description__ = 'Handprint: HANDwritten Page RecognitIoN Test for Caltech Archives'
 __url__         = 'https://github.com/caltechlibrary/handprint'

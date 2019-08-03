6a5e6028358931141a46ffd07d6dc762f34f0352
Michael Hucka <mhucka@caltech.edu>
2019-08-02 22:08:13 -0700

    Move format and service data here instead of constants.py

--- handprint/services/__init__.py
+++ handprint/services/__init__.py
@@ -1,3 +1,18 @@
 from .amazon import AmazonRekognitionTR, AmazonTextractTR
 from .google import GoogleTR
 from .microsoft import MicrosoftTR
+
+ACCEPTED_FORMATS = ('jpg', 'jpeg', 'jp2', 'png', 'gif', 'bmp', 'tif', 'tiff')
+
+KNOWN_SERVICES = {
+    'amazon-rekognition': AmazonRekognitionTR,
+    'amazon-textract': AmazonTextractTR,
+    'google': GoogleTR,
+    'microsoft': MicrosoftTR,
+}
+
+# Save this list to avoid recreating it all the time.
+SERVICES_LIST = sorted(KNOWN_SERVICES.keys())
+
+def services_list():
+    return SERVICES_LIST

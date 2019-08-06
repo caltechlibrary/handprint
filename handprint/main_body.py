9ecb7e23a5f19245213e8e5148451cacbf620cd5
Michael Hucka <mhucka@caltech.edu>
2019-08-06 14:53:45 -0700

    Manager run_service() -> run_services()

--- handprint/main_body.py
+++ handprint/main_body.py
@@ -95,7 +95,7 @@ class MainBody(object):
         for index, item in enumerate(targets, start = 1):
             if print_separators:
                 say.msg('━'*70, 'dark')
-            manager.run_service(item, index, base_name)
+            manager.run_services(item, index, base_name)
         if print_separators:
             say.msg('━'*70, 'dark')
 

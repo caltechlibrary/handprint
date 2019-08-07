2e260fbcf1d767d36f6b8a95b8a5fee21f569c65
Michael Hucka <mhucka@caltech.edu>
2019-08-06 19:22:29 -0700

    Support labeling the figure with the service name

--- handprint/annotate.py
+++ handprint/annotate.py
@@ -24,10 +24,10 @@ from matplotlib.patches import Polygon
 # Main functions.
 # .............................................................................
 
-def annotated_image(file, text_boxes):
+def annotated_image(file, text_boxes, service_name):
     plt.figure(figsize = (20, 20))
     img = mpimg.imread(file)
-    ax = plt.imshow(img, cmap = "gray")
+    plt.imshow(img, cmap = "gray")
     props = dict(facecolor = 'white', alpha = 0.6)
     if text_boxes:
         polygons = [(item.boundingBox, item.text) for item in text_boxes]
@@ -36,6 +36,10 @@ def annotated_image(file, text_boxes):
             text = polygon[1]
             plt.text(vertices[0][0], vertices[0][1], text, color = 'r',
                      fontsize = 8, va = "top", bbox = props)
+    plt.text(0, -15, service_name, size = 'x-large', color = 'red', weight = 'bold')
+    plt.text(0, -30, '.', color = 'white')
+    plt.yticks([])
+    plt.xticks([])
     buf = io.BytesIO()
     plt.savefig(buf, format = 'jpg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
     buf.seek(0)

c92f09eeacddd520e0c72d8feb53e65ab3c305fe
Michael Hucka <mhucka@caltech.edu>
2019-08-08 14:19:07 -0700

    Solve an occasional crasher and also a matplotlib warning
    Running on macos 10.13, i randomly experienced exceptions from
    somewhere deep in the pyplot code *sometimes* when imshow was called
    on the same file.  The actual error was senseless (i.e., it obviously
    didn't indicate the real problem), and only happened when multiple
    threads were used.  At first I thought the problem was due to some
    race condition or resource use condition during multithreaded
    execution, but putting a lock around the imshow() call didn't solve
    it.  Finally I rewrote the way that the plot was created (by adding a
    subplot), and lo and behold, the crashes no longer seem to happen.
    The key seemed to be to create a subplot (and corresponding axes)
    object in which the imshow() is called, rather than calling imshow()
    on the plt object directly.
    
    The other thing solved in this commit is with "fig, axes =
    plt.subplots(...)" it avoids the Matplotlib warning "Adding an axes
    using the same arguments as a previous axes currently reuses the
    earlier instance.  In a future version, a new instance will always be
    created and returned.  Meanwhile, this warning can be suppressed, and
    the future behavior ensured, by passing a unique label to each axes
    instance."

--- handprint/annotate.py
+++ handprint/annotate.py
@@ -18,28 +18,36 @@ import io
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from matplotlib.patches import Polygon
+
+import handprint
+from handprint.debug import log
+from handprint.files import relative
 
 
 # Main functions.
 # .............................................................................
 
 def annotated_image(file, text_boxes, service_name):
-    plt.figure(figsize = (20, 20))
-    img = mpimg.imread(file)
-    plt.imshow(img, cmap = "gray")
+    fig, axes = plt.subplots(nrows = 1, ncols = 1, figsize = (20, 20))
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    axes.set_title(service_name, color = 'r', fontweight = 'bold', fontsize = 14)
+
+    if __debug__: log('reading image file {} for {}', relative(file), service_name)
+    axes.imshow(mpimg.imread(file), cmap = "gray")
+
     props = dict(facecolor = 'white', alpha = 0.6)
     if text_boxes:
+        if __debug__: log('adding {} annotations for {}', len(text_boxes), service_name)
         polygons = [(item.boundingBox, item.text) for item in text_boxes]
         for polygon in polygons:
-            vertices = [(polygon[0][i], polygon[0][i+1]) for i in range(0, len(polygon[0]), 2)]
+            vertices = [(polygon[0][i], polygon[0][i+1])
+                        for i in range(0, len(polygon[0]), 2)]
             text = polygon[1]
             plt.text(vertices[0][0], vertices[0][1], text, color = 'r',
                      fontsize = 8, va = "top", bbox = props)
-    plt.text(0, -15, service_name, size = 'x-large', color = 'red', weight = 'bold')
-    plt.text(0, -30, '.', color = 'white')
-    plt.yticks([])
-    plt.xticks([])
+
+    if __debug__: log('saving annotated {} image for {}', relative(file), service_name)
     buf = io.BytesIO()
     plt.savefig(buf, format = 'jpg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
     buf.seek(0)

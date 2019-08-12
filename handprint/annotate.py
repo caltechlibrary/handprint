61c037b7d32dd0e2a93c19fe5a26929da7637238
Michael Hucka <mhucka@caltech.edu>
2019-08-11 20:45:31 -0700

    Tweak size and positions of text annotations

--- handprint/annotate.py
+++ handprint/annotate.py
@@ -33,7 +33,7 @@ def annotated_image(file, text_boxes, service):
     fig, axes = plt.subplots(nrows = 1, ncols = 1, figsize = (20, 20))
     axes.get_xaxis().set_visible(False)
     axes.get_yaxis().set_visible(False)
-    axes.set_title(service_name, color = 'r', fontweight = 'bold', fontsize = 14)
+    axes.set_title(service_name, color = 'r', fontweight = 'bold', fontsize = 22)
 
     if __debug__: log('reading image file {} for {}', relative(file), service_name)
     axes.imshow(mpimg.imread(file), cmap = "gray")
@@ -41,11 +41,11 @@ def annotated_image(file, text_boxes, service):
     # Basic sanity check against something going really wrong.
     (width, height) = fig.get_size_inches()*fig.dpi     # size in pixels
     if width > 100000 or height > 100000:
-        # This happens occasionally at random.  I think there's some tread
-        # safety issue somewhere but I haven't solved it.
+        # This happens occasionally at random.  I think there's some thread
+        # safety issue causing data corruption somewhere, but I haven't solved it.
         return None
 
-    props = dict(facecolor = 'white', alpha = 0.6)
+    props = dict(facecolor = 'white', alpha = 0.7)
     if text_boxes:
         if __debug__: log('adding {} annotations for {}', len(text_boxes), service_name)
         polygons = [(item.boundingBox, item.text) for item in text_boxes]
@@ -53,8 +53,8 @@ def annotated_image(file, text_boxes, service):
             vertices = [(polygon[0][i], polygon[0][i+1])
                         for i in range(0, len(polygon[0]), 2)]
             text = polygon[1]
-            plt.text(vertices[0][0], vertices[0][1], text, color = 'r',
-                     fontsize = 8, va = "top", bbox = props)
+            plt.text(vertices[0][0] - 4, vertices[0][1] - 8, text, color = 'r',
+                     fontsize = 11, va = "top", bbox = props)
 
     if __debug__: log('saving {} annotated image {}', service_name, relative(file))
     try:

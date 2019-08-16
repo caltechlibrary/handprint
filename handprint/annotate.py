6939d12fef18a7334a3d6a9eab36cec52ecc18a4
Michael Hucka <mhucka@caltech.edu>
2019-08-16 14:17:39 -0700

    Multiple fixes
    1) Switch to PNG as the output format. After days of trying to solve a
    crash involving Matplotlib's AGG backend writing jpeg output in
    multithreaded execusion, I hit on a way to work around the problem (at
    least so far) by switching the backend to something other than AGG.
    However, using jpeg output causes the AGG backend to be invoked no
    matter if another backend is set explicitly.  By switching to PNG,
    this is avoided, and I can finally use a different backend for
    Matplotlib.
    
    2) Make sure text x,y locations are never less than 0.
    
    3) Don't bother putting a try-except wrapper around plt.savefit(); if
    there's an exception, let higher levels catch it.

--- handprint/annotate.py
+++ handprint/annotate.py
@@ -17,10 +17,47 @@ file "LICENSE" for more information.
 import io
 import matplotlib
 import matplotlib.pyplot as plt
+
+# On macOS 10.13.6 with Python 3.5.7 and matplotlib 3.0.3, when running with
+# parallel treads, I experienced random failures in the AGG renderer.  The
+# error always manifested itself as an exception that ended like this, on
+# different input images with different sizes, even though I always ran on the
+# same test images under the same conditions:
+#
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/figure.py", line 2094, in savefig
+#     self.canvas.print_figure(fname, **kwargs)
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backend_bases.py", line 2075, in print_figure
+#     **kwargs)
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backends/backend_agg.py", line 560, in print_jpg
+#     buf, size = self.print_to_buffer()
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backends/backend_agg.py", line 526, in print_to_buffer
+#     FigureCanvasAgg.draw(self)
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backends/backend_agg.py", line 396, in draw
+#     self.renderer = self.get_renderer(cleared=True)
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backends/backend_agg.py", line 417, in get_renderer
+#     self.renderer = RendererAgg(w, h, self.figure.dpi)
+#   File "/Users/mhucka/system/lib/python3.5/site-packages/matplotlib/backends/backend_agg.py", line 87, in __init__
+#     self._renderer = _RendererAgg(int(width), int(height), dpi)
+#   ValueError: Image size of 4896646x5895191 pixels is too large. It must be less than 2^16 in each direction.
+#
+# This is a bogus error -- the image did not have this size.  I tried many
+# things over many days, thinking the problem was in my code or due to
+# threading issues.  Wrapping the savefig() call in threading.lock didn't
+# help.  However, switching the backend currently seems to have stopped the
+# problem.  The tricky thing about switching backends, however, is that not
+# all the backends support all the same output types.  In particular, the
+# Cairo backend does not write JPEG output: when you switch to Cairo and save
+# a plot as a JPG file, it turns out Matplotlib still invokes AGG.  I had to
+# switch not only the backend, but also the output format to PNG (which is
+# a format that Cairo does write) in order to make it all work.
+#
+plt.switch_backend('cairo')
+
 import matplotlib.image as mpimg
 
 import handprint
 from handprint.debug import log
+from handprint.exceptions import *
 from handprint.files import relative
 
 
@@ -35,15 +72,9 @@ def annotated_image(file, text_boxes, service):
     axes.get_yaxis().set_visible(False)
     axes.set_title(service_name, color = 'r', fontweight = 'bold', fontsize = 22)
 
-    if __debug__: log('reading image file {} for {}', relative(file), service_name)
-    axes.imshow(mpimg.imread(file), cmap = "gray")
-
-    # Basic sanity check against something going really wrong.
-    (width, height) = fig.get_size_inches()*fig.dpi     # size in pixels
-    if width > 100000 or height > 100000:
-        # This happens occasionally at random.  I think there's some thread
-        # safety issue causing data corruption somewhere, but I haven't solved it.
-        return None
+    if __debug__: log('reading image file for {}: {}', service_name, relative(file))
+    img = mpimg.imread(file)
+    axes.imshow(img, cmap = "gray")
 
     props = dict(facecolor = 'white', alpha = 0.7)
     if text_boxes:
@@ -52,18 +83,16 @@ def annotated_image(file, text_boxes, service):
         for polygon in polygons:
             vertices = [(polygon[0][i], polygon[0][i+1])
                         for i in range(0, len(polygon[0]), 2)]
+            x = max(0, vertices[0][0] - 4)
+            y = max(0, vertices[0][1] - 8)
             text = polygon[1]
-            plt.text(vertices[0][0] - 4, vertices[0][1] - 8, text, color = 'r',
-                     fontsize = 11, va = "top", bbox = props)
+            plt.text(x, y, text, color = 'r', fontsize = 11, va = "top", bbox = props)
 
-    if __debug__: log('saving {} annotated image {}', service_name, relative(file))
-    try:
-        buf = io.BytesIO()
-        plt.savefig(buf, format = 'jpg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
-        buf.seek(0)
-    except Exception as ex:
-        if __debug__: log('error saving {} annotated image: {}', service_name, str(ex))
-        return None
-    finally:
-        plt.close(fig)
+    if __debug__: log('generating png for {} for {}', service_name, relative(file))
+    buf = io.BytesIO()
+    fig.savefig(buf, format = 'png', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
+    buf.flush()
+    buf.seek(0)
+    plt.close(fig)
+
     return buf

a4f432ffb0c913d157344dbd49fb8a7827626044
Michael Hucka <mhucka@caltech.edu>
2019-12-10 18:14:29 -0800

    Fix bug getting amazon credentials
    The fact there are two Amazon services but only one AmazonCredentials
    object is turning out to be a problem.  AmazonCredentials doesn't know
    which of the two services it's being used with.  This is a design
    error and will need to be fixed properly.  For now, I'm just avoiding
    the problem by creating a fake "amazon" service entry.

--- handprint/credentials/credentials_files.py
+++ handprint/credentials/credentials_files.py
@@ -3,6 +3,7 @@ credentials_files.py: mapping of services to credentials files
 '''
 
 CREDENTIALS_FILES = {
+    'amazon'             : 'amazon_credentials.json',
     'amazon-rekognition' : 'amazon_credentials.json',
     'amazon-textract'    : 'amazon_credentials.json',
     'google'             : 'google_credentials.json',

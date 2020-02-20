b03e85f970122f2777d22bb8068836db4798082c
Michael Hucka <mhucka@caltech.edu>
2020-02-20 15:45:20 -0800

    Update copyright year

--- handprint/debug.py
+++ handprint/debug.py
@@ -9,8 +9,8 @@ Michael Hucka <mhucka@caltech.edu> -- Caltech Library
 Copyright
 ---------
 
-Copyright (c) 2019 by the California Institute of Technology.  This code is
-open-source software released under a 3-clause BSD license.  Please see the
+Copyright (c) 2019-2020 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
--- handprint/exceptions.py
+++ handprint/exceptions.py
@@ -9,7 +9,7 @@ Michael Hucka <mhucka@caltech.edu> -- Caltech Library
 Copyright
 ---------
 
-Copyright (c) 2018-2019 by the California Institute of Technology.  This code
+Copyright (c) 2018-2020 by the California Institute of Technology.  This code
 is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''

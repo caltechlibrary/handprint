70d118a68e49543f5676b32ec218d22d8f9f0df8
mhucka <mhucka@caltech.edu>
2022-03-14 15:38:14 -0700

    Add a basic test for command line arguments

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,5 @@
 import os
+import plac
 import pytest
 import sys
 from   time import time
@@ -9,7 +10,9 @@ try:
 except:
     sys.path.append('..')
 
+from handprint.__main__ import main
 from handprint.exceptions import *
+from handprint.exit_codes import ExitCode
 
 def test_exceptions():
     try:
@@ -17,3 +20,21 @@ def test_exceptions():
     except Exception as ex:
         assert isinstance(ex, HandprintException)
         assert str(ex) == 'foo'
+
+
+def test_good_cli_arg():
+    with pytest.raises(SystemExit) as ex_info:
+        output = plac.call(main, ['-V'])
+        assert isinstance(output, str)
+        assert output.startswith('handprint version')
+
+    assert ex_info.type == SystemExit
+    assert ex_info.value.code == int(ExitCode.success)
+
+
+def test_bad_cli_arg():
+    with pytest.raises(SystemExit) as ex_info:
+        assert plac.call(main, ['-s', 'bogus'])
+
+    assert ex_info.type == SystemExit
+    assert ex_info.value.code == int(ExitCode.bad_arg)
